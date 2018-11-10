830056c35ce9af3139f72bfa9d8f4c34a4468bb1
sairohiththammana <sairohith1212@gmail.com>
2018-11-10 14:42:03 +0530

    tokenizer and bug fix

--- pretrained.py
+++ pretrained.py
@@ -11,12 +11,14 @@ Created on Sat Oct 13 13:14:07 2018
 #4 - positive
 from keras.models import load_model
 import numpy as np
+import pickle as pk
 def sentiment(x):
     number_to_sentiment_mapping = ["Negative" , "Somewhat Negative" , "Neutral" , "Somewhat postive" , "positive"]
     model = load_model('sentimentclassification.h5')     
-    with open('filename.pickle', 'rb') as handle:
+    with open('tokenizer.pickle', 'rb') as handle:
         t = pk.load(handle) 
     x = np.asarray([x])
+    t.oov_token = None
     encoded_texts_test = t.texts_to_sequences(x)
     padded_texts_test = pad_sequences(encoded_texts_test, maxlen=10, padding='post')
     sentiment_index = np.argmax(model.predict(padded_texts_test),axis = 1)[0]
@@ -27,5 +29,5 @@ def sentiment(x):
 
 
 if __name__ == "__main__":
-    print(sentiment("'A series of escapades demonstrating the adage that what is good for the goose is also good for the gander , some of which occasionally amuses but none of which amounts to much of a story .'"))
+    print(sentiment("'A series of escapades leading to bad experience worse"))
     
